924fb98df9187768c81d3912970042ffa9a57777
GabrielCardoz0 <gabriel.scardozo7@gmail.com>
2024-07-27 11:43:45 -0300

    feat: querys from update and delete book by id

--- src/models/books.ts
+++ src/models/books.ts
@@ -16,16 +16,29 @@ async function createBook(newBook: NewBook, userId: number): Promise<QueryResult
     const { name, synopsis, gender, author, serie_name, rating, is_read, img_url } = newBook;
 
     return await db.query(`
-        INSERT INTO
-            books(name, synopsis, gender, author, serie_name, rating, is_read, img_url, user_id)
-        VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9);`,
-    [name, synopsis, gender, author, serie_name, rating, is_read, img_url, userId]);
+        INSERT INTO books(name, synopsis, gender, author, serie_name, rating, is_read, img_url, user_id)
+        VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)
+    ;`, [name, synopsis, gender, author, serie_name, rating, is_read, img_url, userId]);
 };
-
 
 async function getBookById(bookId: number, userId: number) {
     const { rows } = await db.query("SELECT * FROM books WHERE id = $1 AND user_id = $2", [bookId, userId]);
     return rows[0];
+};
+
+async function updateBookById(bookId: number, newBook: NewBook) {
+    return db.query(`
+        UPDATE books
+        SET name = $1, synopsis = $2, gender = $3, author = $4, serie_name = $5, rating = $6, is_read = $7, img_url = $8
+        WHERE id = $9
+    ;`, []);
+};
+
+async function deleteBookById(bookId: number) {
+    return db.query(`
+        DELETE FROM books
+        WHERE id = $1
+    ;`, [bookId]);
 };
 
 const booksModels = {
@@ -33,6 +46,8 @@ const booksModels = {
     getBooksCount,
     createBook,
     getBookById,
+    updateBookById,
+    deleteBookById,
 };
 
 export default booksModels;

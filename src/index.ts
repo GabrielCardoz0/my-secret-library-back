fe172cb2b8b399f75a79d04ac14274aba2e8bbb3
GabrielCardoz0 <gabriel.scardozo7@gmail.com>
2024-07-27 12:14:32 -0300

    feat: jwt autentication middleware for book routes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
-import { JwtVariables } from 'hono/jwt';
+import { jwt, JwtVariables } from 'hono/jwt';
 import env from 'dotenv';
 import { zValidator } from '@hono/zod-validator';
 import { userCredentialsSchema } from './schemas/auth';
 import authController from './controllers/auth';
 import booksController from './controllers/books';
 import { newBookSchema } from './schemas/books';
+import { bearerAuth } from 'hono/bearer-auth';
 
 
 type Variables = JwtVariables
@@ -14,12 +15,13 @@ type Variables = JwtVariables
 
 env.config();
 
-const { PORT } = process.env;
+const { PORT, JWT_SECRET } = process.env;
 
 const app = new Hono<{ Variables: Variables }>()
 
 app
   .post('/auth', zValidator("json", userCredentialsSchema), authController.signIn)
+  .use('/books/*', jwt({ secret: JWT_SECRET! }))
   .get('/books', booksController.getBooks)
   .post('/books', zValidator("json", newBookSchema), booksController.creteBook)
   .get('/books/:bookId', booksController.getBookById)
